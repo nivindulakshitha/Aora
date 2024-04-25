206b7572ece073df74e6c2771eb53e61b50fae19
codingdesk-dev <mail.codingdesk@gmail.com>
2024-04-25 13:55:33 +0530

    Add SplashScreen and custom fonts in app/index.jsx, remove app/profile.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,36 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
-import { Link } from "expo-router";
+import { Link, SplashScreen } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+	const [fontsLoaded, error] = useFonts({
+		"Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+		"Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+		"Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+		"Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+		"Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+		"Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+		"Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+		"Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+		"Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+	});
+
+	useEffect(() => {
+		if (error) throw error;
+		if (fontsLoaded) SplashScreen.hideAsync();
+	}, [fontsLoaded, error]);
+
+	if (!fontsLoaded && !error) return null;
+
 	return (
 		<View className="bg-white flex-1 items-center justify-center">
-			<Text className="text-3xl">Aora welcome!</Text>
+			<Text className="text-3xl font-pblack">Aora welcome!</Text>
 			<StatusBar style="auto" />
-			<Link href="/profile" style={{ color: "blue" }}>
+			<Link href="/home" style={{ color: "blue" }}>
 				Go to Profile
 			</Link>
 		</View>
