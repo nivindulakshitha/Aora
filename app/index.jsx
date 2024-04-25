d7dfbffa2ca24d6f0143bd4c95d95c1ad4927dbd
codingdesk-dev <mail.codingdesk@gmail.com>
2024-04-25 15:14:09 +0530

    Refactor app/index.jsx to update welcome screen UI and add SafeAreaView

--- app/index.jsx
+++ app/index.jsx
@@ -1,8 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-import { Link, SplashScreen } from "expo-router";
+import { Image, ScrollView, Text, View } from "react-native";
+import { Redirect, router, SplashScreen } from "expo-router";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import CustomButton from "../components/buttons";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -27,12 +30,42 @@ export default function App() {
 	if (!fontsLoaded && !error) return null;
 
 	return (
-		<View className="bg-white flex-1 items-center justify-center">
-			<Text className="text-3xl font-pblack">Aora welcome!</Text>
-			<StatusBar style="auto" />
-			<Link href="/home" style={{ color: "blue" }}>
-				Go to Profile
-			</Link>
-		</View>
+		<SafeAreaView className="bg-primary h-full">
+			<ScrollView contentContainerStyle={{ height: "100%" }}>
+				<View className="w-full h-full justify-center items-center p-4">
+					<Image
+						source={images.logo}
+						className="w-[130px] h-[84px]"
+						resizeMode="contain"
+					/>
+					<Image
+						source={images.cards}
+						className="w-full h-[300px]"
+						resizeMode="contain"
+					/>
+
+					<View className="relative mt-5">
+						<Text className="text-3xl text-white font-pbold text-center">
+							Discover Endless {"\n"} Possibilities with
+							<Text className="text-secondary-200"> Aora</Text>
+						</Text>
+						<Image
+							source={images.path}
+							className="w-[55px] h-[15px] absolute bottom-0 right-1 -z-10"
+							resizeMode="contain"
+						/>
+					</View>
+
+					<Text className="text-gray-100 text-sm font-pregular text-center mt-7">
+						Where Creativity Meets Innovation: Embark on a Journey {"\n"}of
+						Limitless Exploration with Aora
+					</Text>
+
+					<CustomButton title='Continue with Email' handlePress={() => router.push('/sign-in')} containerStyle='w-full mt-7' />
+				</View>
+			</ScrollView>
+
+			<StatusBar backgroundColor="#161622" style="light" />
+		</SafeAreaView>
 	);
 }
---
+++ components/buttons.jsx
@@ -0,0 +1,19 @@
+import { TouchableOpacity, Text } from "react-native";
+import React from "react";
+
+const CustomButton = ({ title, handlePress, containerStyle, textStyle, isLoading }) => {
+	return (
+		<TouchableOpacity
+			className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyle} ${
+				isLoading ? "opacity-50" : "opacity-100"
+                }`}
+            disabled={ isLoading}
+			onPress={handlePress}
+			activeOpacity={0.85}
+		>
+            <Text className={`text-primary font-psemibold text-xl ${textStyle}`}>{title}</Text>
+		</TouchableOpacity>
+	);
+};
+
+export default CustomButton;
